7aaef78d2ac1df6353248e42c3425df9c2d658d3
pwpdev <pramod@wbcomdesigns.com>
2018-10-23 13:46:42 +0530

    icons colors issue managed

--- modules/notification-area/widgets/notification-area.php
+++ modules/notification-area/widgets/notification-area.php
@@ -111,7 +111,7 @@ class NotificationArea extends Widget_Base {
 				'type'		 => \Elementor\Controls_Manager::COLOR,
 				'default'	 => '#ffffff',
 				'selectors'	 => [
-					'{{WRAPPER}} .header-right.no-gutter.wb-grid-flex.grid-center .rg-search-icon.icon-search-interface-symbol, {{WRAPPER}} .header-right.no-gutter.wb-grid-flex.grid-center .rg-icon-wrap, {{WRAPPER}} .header-right.no-gutter.wb-grid-flex.grid-center .rg-icon-wrap a' => 'color: {{VALUE}};',
+					'{{WRAPPER}} .header-right.no-gutter.wb-grid-flex.grid-center .rg-search-icon.icon-search-interface-symbol, {{WRAPPER}} .header-right.no-gutter.wb-grid-flex.grid-center .rg-icon-wrap, {{WRAPPER}} .header-right.no-gutter.wb-grid-flex.grid-center .rg-icon-wrap a, {{WRAPPER}} .header-right.no-gutter.wb-grid-flex.grid-center .rg-icon-wrap span:before, {{WRAPPER}} .header-right.no-gutter.wb-grid-flex.grid-center .rg-search-icon:before' => 'color: {{VALUE}};',
 				],
 			]
 		);
@@ -122,7 +122,8 @@ class NotificationArea extends Widget_Base {
 				'type'		 => \Elementor\Controls_Manager::COLOR,
 				'default'	 => '#ffffff',
 				'selectors'	 => [
-					'{{WRAPPER}} .header-right.no-gutter.wb-grid-flex.grid-center .rg-search-icon.icon-search-interface-symbol:hover, {{WRAPPER}} .header-right.no-gutter.wb-grid-flex.grid-center .rg-icon-wrap:hover, {{WRAPPER}} .header-right.no-gutter.wb-grid-flex.grid-center .rg-icon-wrap a:hover' => 'color: {{VALUE}};',
+					'{{WRAPPER}} .header-right.no-gutter.wb-grid-flex.grid-center .rg-search-icon.icon-search-interface-symbol:hover, {{WRAPPER}} .header-right.no-gutter.wb-grid-flex.grid-center .rg-icon-wrap:hover, {{WRAPPER}} .header-right.no-gutter.wb-grid-flex.grid-center .rg-icon-wrap a:hover,
+					{{WRAPPER}} .header-right.no-gutter.wb-grid-flex.grid-center .rg-icon-wrap span:hover:before, {{WRAPPER}} .header-right.no-gutter.wb-grid-flex.grid-center .rg-search-icon:hover:before' => 'color: {{VALUE}};',
 				],
 			]
 		);
