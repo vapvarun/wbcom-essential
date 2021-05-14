6c9777bce29d7053534b23e72124ad833e807cf6
dishantpatel <dishant.patel1007@gmail.com>
2021-05-14 18:03:44 +0530

    Fixed Member Swiper Carousel slider

--- plugins/elementor/assets/js/member-carousel.js
+++ plugins/elementor/assets/js/member-carousel.js
@@ -1,98 +1,93 @@
-class MemberCarousel extends elementorModules.frontend.handlers.Base {
+( function( $ ) {
+	"use strict";
+	
+	
+	var wbcom_memberCarousel = function() {
+		
+		jQuery( '.member-carousel-container' ).each( function() {			
+			
+			var elementSettings = $(this).parent().parent().data( 'settings' ),
+				slidesToShow = +elementSettings.slides_to_show || 3,
+				isSingleSlide = 1 === slidesToShow,
+				defaultLGDevicesSlidesCount = isSingleSlide ? 1 : 2,
+				elementorBreakpoints = elementorFrontend.config.responsive.activeBreakpoints;
+			
+			var swiperOptions = {
+				slidesPerView: slidesToShow,
+				loop: 'yes' === elementSettings.infinite,
+				speed: elementSettings.speed,
+				handleElementorBreakpoints: true
+			};
+			swiperOptions.breakpoints = {};
+			swiperOptions.breakpoints[elementorBreakpoints.mobile.value] = {
+				slidesPerView: +elementSettings.slides_to_show_mobile || 1,
+				slidesPerGroup: +elementSettings.slides_to_scroll_mobile || 1
+			};
+			swiperOptions.breakpoints[elementorBreakpoints.tablet.value] = {
+				slidesPerView: +elementSettings.slides_to_show_tablet || defaultLGDevicesSlidesCount,
+				slidesPerGroup: +elementSettings.slides_to_scroll_tablet || 1
+			};
 
-  getDefaultSettings() {
-    return {
-      selectors: {
-        carousel: '.member-carousel-container',
-        slideContent: '.swiper-slide'
-      },
-    };
-  }
+			if ('yes' === elementSettings.autoplay) {
+				swiperOptions.autoplay = {
+					delay: elementSettings.autoplay_speed,
+					disableOnInteraction: 'yes' === elementSettings.pause_on_interaction
+				};
+			}
 
-  getDefaultElements() {
-    var selectors = this.getSettings('selectors');
-    var elements = {
-      $slider: this.$element.find(selectors.carousel)
-    };
-    elements.$mainSwiperSlides = elements.$slider.find(selectors.slideContent);
-    return elements;
-  }
+			if (isSingleSlide) {
+				swiperOptions.effect = elementSettings.effect;
 
-  getSwiperSettings() {
-    var elementSettings = this.getElementSettings(),
-      slidesToShow = +elementSettings.slides_to_show || 3,
-      isSingleSlide = 1 === slidesToShow,
-      defaultLGDevicesSlidesCount = isSingleSlide ? 1 : 2,
-      elementorBreakpoints = elementorFrontend.config.responsive.activeBreakpoints;
+			  if ('fade' === elementSettings.effect) {
+				swiperOptions.fadeEffect = {
+					crossFade: true
+				};
+			  }
+			} else {
+				.slidesPerGroup = +elementSettings.slides_to_scroll || 1;
+			}
 
-    console.log(elementSettings);
-    var swiperOptions = {
-      slidesPerView: slidesToShow,
-      loop: 'yes' === elementSettings.infinite,
-      speed: elementSettings.speed,
-      handleElementorBreakpoints: true
-    };
-    swiperOptions.breakpoints = {};
-    swiperOptions.breakpoints[elementorBreakpoints.mobile.value] = {
-      slidesPerView: +elementSettings.slides_to_show_mobile || 1,
-      slidesPerGroup: +elementSettings.slides_to_scroll_mobile || 1
-    };
-    swiperOptions.breakpoints[elementorBreakpoints.tablet.value] = {
-      slidesPerView: +elementSettings.slides_to_show_tablet || defaultLGDevicesSlidesCount,
-      slidesPerGroup: +elementSettings.slides_to_scroll_tablet || 1
-    };
+			if (elementSettings.image_spacing_custom) {
+				swiperOptions.spaceBetween = elementSettings.image_spacing_custom.size;
+			}
 
-    if ('yes' === elementSettings.autoplay) {
-      swiperOptions.autoplay = {
-        delay: elementSettings.autoplay_speed,
-        disableOnInteraction: 'yes' === elementSettings.pause_on_interaction
-      };
-    }
+			var showArrows = 'arrows' === elementSettings.navigation || 'both' === elementSettings.navigation,
+				showDots = 'dots' === elementSettings.navigation || 'both' === elementSettings.navigation;
 
-    if (isSingleSlide) {
-      swiperOptions.effect = elementSettings.effect;
+			if (showArrows) {
+				.navigation = {
+					prevEl: '.elementor-swiper-button-prev',
+					nextEl: '.elementor-swiper-button-next'
+				};
+			}
 
-      if ('fade' === elementSettings.effect) {
-        swiperOptions.fadeEffect = {
-          crossFade: true
-        };
-      }
-    } else {
-      swiperOptions.slidesPerGroup = +elementSettings.slides_to_scroll || 1;
-    }
+			if (showDots) {
+				swiperOptions.pagination = {
+					el: '.swiper-pagination',
+					type: 'bullets',
+					clickable: true
+				};
+			}
+			/* */
+			
+			var member_swiper =	new Swiper($(this), swiperOptions );
+			
+			if ( elementSettings.pause_on_hover == 'yes' ) {
+				$( this ).mouseenter(function() {
+					member_swiper.autoplay.stop();
+					console.log('slider stopped');
+				});
 
-    if (elementSettings.image_spacing_custom) {
-      swiperOptions.spaceBetween = elementSettings.image_spacing_custom.size;
-    }
+				$( this ).mouseleave(function() {
+					member_swiper.autoplay.start();
+					console.log('slider started again');
+				});
+			}
+		});
+	}	
+	
+	jQuery(window).on('elementor/frontend/init', () => {	 
+		elementorFrontend.hooks.addAction('frontend/element_ready/wbcom-members-carousel.default', wbcom_memberCarousel);
+	});
 
-    var showArrows = 'arrows' === elementSettings.navigation || 'both' === elementSettings.navigation,
-      showDots = 'dots' === elementSettings.navigation || 'both' === elementSettings.navigation;
-
-    if (showArrows) {
-      swiperOptions.navigation = {
-        prevEl: '.elementor-swiper-button-prev',
-        nextEl: '.elementor-swiper-button-next'
-      };
-    }
-
-    if (showDots) {
-      swiperOptions.pagination = {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true
-      };
-    }
-
-    return swiperOptions;
-  }
-
-}
-
-jQuery(window).on('elementor/frontend/init', () => {
-  const _memberCarousel = ($element) => {
-    elementorFrontend.elementsHandler.addHandler(MemberCarousel, {
-      $element,
-    });
-  };
-  elementorFrontend.hooks.addAction('frontend/element_ready/wbcom-members-carousel.default', _memberCarousel);
-});
+})( jQuery );
