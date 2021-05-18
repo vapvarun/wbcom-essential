db975cd915e7ce911b049d2aec358ca4b1636bef
Varun Kumar Dubey <vapvarun@gmail.com>
2021-05-18 12:10:40 +0530

    Update loader.php

--- loader.php
+++ loader.php
@@ -14,8 +14,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-define( 'WBCOM_ESSENTIAL_VERSION', '3.0.0' );
-define( 'WBCOM_ESSENTIAL_PREVIOUS_STABLE_VERSION', '3.0.0' );
+define( 'WBCOM_ESSENTIAL_VERSION', '3.1.0' );
+define( 'WBCOM_ESSENTIAL_PREVIOUS_STABLE_VERSION', '3.1.0' );
 
 define( 'WBCOM_ESSENTIAL_FILE', __FILE__ );
 define( 'WBCOM_ESSENTIAL_PLUGIN_BASE', plugin_basename( WBCOM_ESSENTIAL_FILE ) );
