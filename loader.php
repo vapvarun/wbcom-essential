e3172a699fbc2c652df87cd2172d48deb68b693b
dishantpatel <dishant.patel1007@gmail.com>
2021-05-20 16:57:57 +0530

    Deactivate Plugin when Reign, BuddyX and BuddyxPro theme not activate

--- loader.php
+++ loader.php
@@ -50,3 +50,33 @@ add_action( 'plugins_loaded', 'wbcom_essential_load_plugin_textdomain' );
  * @since 3.0.0
  */
 wbcom_essential();
+
+
+/**
+ *  Check if Reign, BuddyX, BuddyxPro theme activate.
+ */
+add_action( 'admin_init', 'wbcom_essential_requires_wbcom_themes' );
+function wbcom_essential_requires_wbcom_themes() {
+	$template = get_option( 'template' );
+	$themes = apply_filters( 'wbcom_essential_themes', array( 'reign-theme', 'buddyx', 'buddyx-pro' ) );
+	
+	if ( !in_array( $template, $themes) ) {
+        deactivate_plugins( plugin_basename( __FILE__ ) );        
+        add_action( 'admin_notices', 'wbcom_essential_required_theme_admin_notice' );
+        unset($_GET['activate']);
+    }
+	
+}
+
+function wbcom_essential_required_theme_admin_notice(){
+
+    $wbcom_essential_plugin	= esc_html__(' Wbcom Essential', 'wbcom-essential');
+    $themes_name 			= apply_filters( 'wbcom_essential_themes_name', array( 'Reign', 'BuddyX', 'BuddyxPro' ) );
+	$themes_name 			= join(", ",$themes_name);
+    echo '<div class="error"><p>';
+    echo sprintf(esc_html__('%1$s is ineffective now as it requires %2$s to be installed and active.', 'buddypress-moderation-pro'), '<strong>' . esc_html($wbcom_essential_plugin) . '</strong>', '<strong>' . esc_html($themes_name) . '</strong>');
+    echo '</p></div>';
+    if (isset($_GET['activate']) ) {
+        unset($_GET['activate']);
+    }
+}
