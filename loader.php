d2d66d6647a178b59d88dc2d7da49cb16f48a1f3
Varun Kumar Dubey <vapvarun@gmail.com>
2021-05-20 16:26:12 +0530

    Update loader.php

--- loader.php
+++ loader.php
@@ -4,7 +4,7 @@
  * Description: Wbcom Essential Addons.
  * Plugin URI: https://wbcomdesigns.com/
  * Author: Wbcom Designs
- * Version: 3.1.0
+ * Version: 3.2.0
  * Author URI: https://wbcomdesigns.com/
  *
  * Text Domain: wbcom-essential
@@ -14,8 +14,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-define( 'WBCOM_ESSENTIAL_VERSION', '3.1.0' );
-define( 'WBCOM_ESSENTIAL_PREVIOUS_STABLE_VERSION', '3.1.0' );
+define( 'WBCOM_ESSENTIAL_VERSION', '3.2.0' );
+define( 'WBCOM_ESSENTIAL_PREVIOUS_STABLE_VERSION', '2.7.0' );
 
 define( 'WBCOM_ESSENTIAL_FILE', __FILE__ );
 define( 'WBCOM_ESSENTIAL_PLUGIN_BASE', plugin_basename( WBCOM_ESSENTIAL_FILE ) );
