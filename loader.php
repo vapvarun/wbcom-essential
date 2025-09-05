46a274e793d6f0891ec5988c1d1700d9fd2a212f
Nitin Patil <pallavi@wbcomdesigns.com>
2025-09-05 16:09:12 +0530

    Update website and plugin url
    https://3.basecamp.com/5798509/buckets/37595336/card_tables/cards/8972087489

--- loader.php
+++ loader.php
@@ -12,11 +12,11 @@
  *
  * @wordpress-plugin
  * Plugin Name:       WBcom Essential
- * Plugin URI:        https://wbcomdesigns.com
+ * Plugin URI:        https://wbcomdesigns.com/downloads/wbcom-essential/
  * Description:       Premium Elementor widgets for BuddyPress, WooCommerce, and WordPress. Create stunning websites with 40+ professional widgets and seamless integrations.
  * Version:           4.0.0
  * Author:            WBcom Designs
- * Author URI:        https://wbcomdesigns.com/plugins
+ * Author URI:        https://wbcomdesigns.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       wbcom-essential
