64a3956c32a92da54eec51cdf77be61820eb22a5
Nitin Patil <pallavi@wbcomdesigns.com>
2026-01-07 19:29:03 +0530

    Manage members carousel and color options
    https://3.basecamp.com/5798509/buckets/37595336/card_tables/cards/9300330540

--- build/blocks/members-carousel/index.asset.php
+++ build/blocks/members-carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '34044970bf257a696e84');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd94a3c17359dab46ff7d');
--- plugins/gutenberg/blocks/members-carousel/members-carousel.php
+++ plugins/gutenberg/blocks/members-carousel/members-carousel.php
@@ -10,6 +10,34 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
+ * Enqueue Swiper for Members Carousel block.
+ */
+function wbcom_essential_members_carousel_enqueue_scripts() {
+	// Only enqueue if the block is present.
+	if ( ! has_block( 'wbcom-essential/members-carousel' ) ) {
+		return;
+	}
+
+	// Enqueue Swiper script.
+	wp_enqueue_script(
+		'swiper',
+		WBCOM_ESSENTIAL_URL . 'assets/vendor/swiper/swiper-bundle.min.js',
+		array(),
+		WBCOM_ESSENTIAL_VERSION,
+		true
+	);
+
+	// Enqueue Swiper CSS.
+	wp_enqueue_style(
+		'wbcom-swiper-css',
+		WBCOM_ESSENTIAL_URL . 'assets/vendor/swiper/swiper-bundle.min.css',
+		array(),
+		WBCOM_ESSENTIAL_VERSION
+	);
+}
+add_action( 'wp_enqueue_scripts', 'wbcom_essential_members_carousel_enqueue_scripts' );
+
+/**
  * Registers the block using the metadata loaded from block.json.
  */
 function wbcom_essential_members_carousel_block_init() {
