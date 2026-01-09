29556f8a7d8eee7a292d1bfc81d68e648cccd260
Nitin Patil <pallavi@wbcomdesigns.com>
2026-01-09 17:09:59 +0530

    Manage dashboard intro block name color
    https://3.basecamp.com/5798509/buckets/37595336/card_tables/cards/9409963540

--- build/blocks/dashboard-intro/index.asset.php
+++ build/blocks/dashboard-intro/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '32cb4c9ffbb29d05cc86');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b3f7853ac1bffdd285b6');
