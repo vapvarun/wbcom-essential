07f7eab6d9742e0d05435420145ed65409e6ff36
Nitin Patil <pallavi@wbcomdesigns.com>
2026-01-13 18:14:54 +0530

    Manage header bar cart and search UI
    https://3.basecamp.com/5798509/buckets/37595336/card_tables/cards/9455944148

--- build/blocks/header-bar/index.asset.php
+++ build/blocks/header-bar/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8b5dceab530fd601ed95');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '1aeb860c936191c2eed7');
