e58930261254dd2717d8725197f92d402a360078
Varun Kumar Dubey <vapvarun@gmail.com>
2026-07-06 10:53:11 +0530

    4.6.3: bump gruntfile pluginVersion to match release

--- gruntfile.js
+++ gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function (grunt) {
 	// Project configuration.
 	// Keep pluginVersion in sync with loader.php / readme.txt when releasing.
 	var pluginSlug    = 'wbcom-essential';
-	var pluginVersion = '4.6.2';
+	var pluginVersion = '4.6.3';
 
 	grunt.initConfig(
 		{
